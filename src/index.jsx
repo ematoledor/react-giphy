a917b1b1edeefd63352093998a3ece9852ec2d80
Emanuel Toledo <ematolr@gmail.com>
2020-07-19 12:43:30 -0500

    created github repo, building first component

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,{name}
+//     </div>
+//   );
+// };
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+// const root = document.getElementById('root');
+// if (root) {
+//   ReactDOM.render(<Hello name="World" />, root);
+// }
+
+const Hello = (props) => {
+  return <h1>Hello, {props.name}</h1>;
 }
+
+const helloBoris = <Hello name="Boris" />
