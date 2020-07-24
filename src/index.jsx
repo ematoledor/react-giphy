f12a226095c691bcc1fb5e533d25c8fc1b9323f2
Emanuel Toledo <ematolr@gmail.com>
2020-07-23 21:49:21 -0500

    trying functons

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-// const Hello = ({ name }) => {
-//   return (
-//     <div>
-//       Hello,{name}
-//     </div>
-//   );
-// };
+const Hello = ({ name }) => {
+  return (
+    <div>
+      Hello, 
+      {name}
+    </div>
+  );
+};
 
-// const root = document.getElementById('root');
-// if (root) {
-//   ReactDOM.render(<Hello name="World" />, root);
-// }
-
-const Hello = (props) => {
-  return <h1>Hello, {props.name}</h1>;
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.render(<Hello name="Ema" />, root);
 }
-
-const helloBoris = <Hello name="Boris" />
